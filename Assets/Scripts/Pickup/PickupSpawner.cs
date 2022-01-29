f80ea4df9e5d8fbce7b81a60470fbc9aa2bbc7c7
Alexander Bozhinov <bgnocturnal@gmail.com>
2022-01-29 16:52:55 +0200

    * Added PickupSpawner prefab * Added PickupManager script to GameManager object * Added a PH_Pickup prefab

---
+++ Assets/Scripts/Pickup/PickupSpawner.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private Transform myTransform;
+
+    [SerializeField]
+    private GameObject spawnedPickup;
+
+    [SerializeField]
+    private bool spawnOnStart;
+
+    private void Start() {
+        if (spawnOnStart) {
+            Spawn();
+        }
+    }
+
+    public void Spawn() {
+        Instantiate(spawnedPickup, myTransform.position, myTransform.rotation, myTransform);
+    }
+}
